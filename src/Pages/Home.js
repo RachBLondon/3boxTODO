ab0f2b647ae0a9885cb0ff2b7738a889a1484356
Rachel <rachblondon@gmail.com>
2020-06-18 11:22:43 +0200

    Profile hover name shown

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,7 +8,7 @@ export default class Home extends Component {
         <h5 style={{textAlign : "center"}}>Welcome</h5>
         <div style={{width : '180px', margin : "auto", height : '100px'}}>
 
-          <ProfileHover address={this.props.ethAddress} noTheme={true}/>
+          <ProfileHover address={this.props.ethAddress} noTheme={true} showName={true}/>
         </div>
         {(!this.props.space)&& <div style={{display : "block", margin : "auto", width : '50px'}}>
               <BounceLoader color={'#D8EEEC'} />
